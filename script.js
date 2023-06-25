cb90d19b37b7d96dca632f471846b5ea42d2e8ac
Dave <102557749+KaluDavid@users.noreply.github.com>
2023-06-26 00:17:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "head") {
+    x.className += " responsive";
+  } else {
+    x.className = "head";
+  }
+}
+
+
+
+
+
+
